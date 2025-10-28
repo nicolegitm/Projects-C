e3ace8aab97f8ae3e7c417fb388adb0a6101b8da
Nicole <nicole.marchenko11@gmail.com>
2025-10-28 14:40:14 -0400

    Create main.c

---
+++ games/numberGuessing/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int secretNumber, guess, attempts = 0;
+
+    srand(time(NULL));
+    secretNumber = rand() % 100 + 1; 
+
+    printf("Welcome to the Number Guessing Game!\n");
+    printf("Choose a number between 1 and 100.\n");
+
+    do {
+        printf("Enter your guess: ");
+        scanf("%d", &guess);
+        attempts++;
+
+        if (guess < secretNumber) {
+            printf("Too low! Try again.\n");
+        } else if (guess > secretNumber) {
+            printf("Too high! Try again.\n");
+        } else {
+            printf("Correct! You guessed the number in %d attempts.\n", attempts);
+        }
+
+    } while (guess != secretNumber);
+
+    return 0;
+}
